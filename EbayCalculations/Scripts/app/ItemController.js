54ee132dc337874f15392a67e546864a76e0aac3
Thomas Campbell-Adams <v-thcamp@microsoft.com>
2017-05-23 17:11:09 -0700

    *Adding sorting controls to table *Adding styling to table.

--- EbayCalculations/Scripts/app/ItemController.js
+++ EbayCalculations/Scripts/app/ItemController.js
@@ -3,20 +3,64 @@
         function($scope, $http) {
             $scope.title = "loading items...";
             $scope.options = [];
-            $scope.working = false;
+            $scope.working = true;
             $scope.items = [];
+            $scope.summations = {
+                price: 0,
+                shippingPaidByBuyer: 0,
+                shippingPaidBySeller: 0,
+                shippingContainerPrice: 0,
+                paypalFee: 0,
+                ebayFee: 0
+            };
+
+            function resetSummations() {
+                $scope.summations = {
+                    price: 0,
+                    shippingPaidByBuyer: 0,
+                    shippingPaidBySeller: 0,
+                    shippingContainerPrice: 0,
+                    paypalFee: 0,
+                    ebayFee: 0
+                };
+            };
+
+            function calculateSummations() {
+                var intId = setInterval(function () {
+                    if (!$scope.working) {
+                        for (var i = 0, len = $scope.items.length; i < len; i++) {
+                            var item = $scope.items[i];
+
+                            $scope.summations.price += item.price;
+                            $scope.summations.shippingPaidByBuyer += item.shippingPaidByBuyer;
+                            $scope.summations.shippingPaidBySeller += item.shippingPaidBySeller;
+                            $scope.summations.shippingContainerPrice += item.shippingContainerPrice;
+                            $scope.summations.paypalFee += item.paypalFee;
+                            $scope.summations.ebayFee += item.ebayFee;
+                        }
+
+                        $scope.$apply();
+                        clearInterval(intId);
+                    }
+                }, 50);
+            }
 
             $scope.getAllItems = function () {
                 $scope.working = true;
 
                 $http.get('/api/items', { headers: { Authorization: 'Bearer ' + sessionStorage.accessToken } })
                     .then(function (response, status, headers, config) {
-                        $scope.working = false;
                         $scope.items = response.data;
+                        $scope.working = false;
                     }, function (data, status, headers, config) {
                         $scope.title = "Oops... something went wrong";
                         $scope.working = false;
                     });
+            };
+
+            $scope.getSummations = function () {
+                resetSummations();
+                calculateSummations();
             };
 
             $scope.sendItem = function(option) {
